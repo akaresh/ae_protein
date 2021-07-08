f1be6cc72f92fe5f54bd8c30cf27042bd79384fa
xperthunter <kjfraga@ucdavis.ed>
2021-07-08 11:05:13 -0700

    training/learning notes

--- trainers/frag_autoencoder.py
+++ trainers/frag_autoencoder.py
@@ -19,4 +19,27 @@ print(df.head(3))
 print(df.columns)
 print(df.shape)
 
+"""
+- train.py --model --batchsize --epochs --splits --df
+- models/ FragmentAutoencoder.py
+	- models are dynamically constructed
+	- fully connected multi-layer autoencoders
+		- flatten the coordinate matrix
+		- (7x3) initial size
+		- convolutions on the 7x3 or
+		- convolutions on the distance matrix
+		- 7x7 matrix 
+		- normalize distance values between 0-1
 
+1. FC with flat coordinates
+2. CNN with matrix coordinates
+3. CNN with distance matrix
+
+conceptual pt
+learn lower dimensional representations of local structure
+21->10->2->10->21
+21->200->10->2->10->200->21
+
+how are we going to test
+
+"""
