22cd5a078a225f1a00caf2352dee6800baa45ed6
akaresh <akaresh@ucdavis.edu>
2021-07-15 13:20:10 -0700

    changed the format

--- visualization/pdb_generator.py
+++ visualization/pdb_generator.py
@@ -14,47 +14,77 @@ import Bio.Data.IUPACData as conv
 # print(pd.__version__)
 # sys.exit()
 
-parser = argparse.ArgumentParser(description='making fragments')
-parser.add_argument('--atoms', '-a', required=False, type=str, default=None,
-	metavar='<path>', help='path to prebuilt atom data frame')
-parser.add_argument('--cifs', '-c', required=False, type=str,
-	metavar='<path>', help='path to file containing set of cifs to build')
+if __name__ == '__main__':
 
-arg = parser.parse_args()
+	parser = argparse.ArgumentParser(description='making fragments')
+	parser.add_argument('--atoms', '-a', required=False, type=str, default=None,
+		metavar='<path>', help='path to prebuilt atom data frame')
+	parser.add_argument('--cifs', '-c', required=False, type=str,
+		metavar='<path>', help='path to file containing set of cifs to build')
 
-assert(arg.atoms != None)
+	arg = parser.parse_args()
 
-#converting to 3 letter code for pdb file
-aa_dict = conv.protein_letters_1to3_extended
+	assert(arg.atoms != None)
+
+	#converting to 3 letter code for pdb file
+	aa_dict = conv.protein_letters_1to3_extended
 
 
-atm_frame = pd.read_pickle(arg.atoms, compression='xz')
+	frag_frame = pd.read_pickle(arg.atoms, compression='xz')
 
-#writing in consideration only for CA dataframe
-for idx, row in atm_frame.iterrows():
-	pdb_to_list = []
+	#writing in consideration only for CA dataframe
+	for idx, row in frag_frame.iterrows():
+		# for i in row['xyz_set']:
+# 			print(i)
+# 		sys.exit()
+		pdb_to_list = []
 	
-	for f_id, r, c in zip(row['fragment_ids'], row['fragment_seq'], row['xyz_set']):
-							#atom, fragment_id, atom_type, residue_3_letter_code, chain name, 
-							#residue number, coordinates
-		pdb_to_list.append(['ATOM', f_id, row['fragment_type'], aa_dict[r].upper(), 
-							row['chain_id'], f_id, c[0], c[1], c[2]])
-		
-		print(pdb_to_list)
+		for f_id, r, c in zip(row['fragment_ids'], row['fragment_seq'], row['xyz_set']):
+								#atom, fragment_id, atom_type, residue_3_letter_code, chain name, 
+								#residue number, coordinates
+			pdb_to_list.append(['ATOM', (f_id), row['fragment_type'], aa_dict[r].upper(), 
+								row['chain_id'], (f_id), (c[0]), (c[1]), (c[2])])
+			
+		for a in pdb_to_list:
+			begin = f'ATOM  {a[1]:>5} {a[2]:<4} {a[3]:>3} {a[4]}{a[5]:>4}    '
+			coords = f'{a[6]:>8.3f}{a[7]:>8.3f}{a[8]:>8.3f}'
+			end = "  1.00  0.00           C  "
+			print(begin+coords+end)
+		sys.exit()
+			
+			
+			
+			#print(pdb_to_list)
 	
-	filepdb = open(f'f{idx}.pdb', 'w')
-	for l in pdb_to_list:
-		filepdb.write(str(l)+'\n')
-		print(l)
-	filepdb.close()
+	'''
+	
+		#filepdb = open(f'f{idx}.pdb', 'w')
+			
+		# for l in pdb_to_list:
+# 			filepdb.write('   '.join(l)+'\n')
+			
+			
+			
+			 line = "%s%8.3f%8.3f%8.3f%s" % (begin,atom[2],atom[3],atom[4],end)
+				  print(line,file=f,end='')
+			else:
+			  for atom in atoms:
+				begin = "ATOM %6d %2d   R00     1    " % (atom[0],atom[1])
+				middle = "%8.3f%8.3f%8.3f" %  (atom[2],atom[3],atom[4])
+				end = "  1.00  0.00    NONE"
+				print(begin+middle+end,file=f)
+			
+			
+		# filepdb.close()
 		
-	sys.exit()
+		sys.exit()
 
-# Example: saving a structure
+	# Example: saving a structure
 
 
-# >>> io = PDBIO()
-# >>> io.set_structure(s)
-# >>> io.save("out.pdb")
+	# >>> io = PDBIO()
+	# >>> io.set_structure(s)
+	# >>> io.save("out.pdb")
 
-#https://fitzkee.chemistry.msstate.edu/sites/default/files/ch8990/pymol-tutorial.pdf
+	#https://fitzkee.chemistry.msstate.edu/sites/default/files/ch8990/pymol-tutorial.pdf
+	'''
