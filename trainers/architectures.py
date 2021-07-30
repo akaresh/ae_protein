5d10bb44d280731b7f1614e08e1c433a8bce7866
xperthunter <kjfraga@ucdavis.edu>
2021-07-29 23:30:53 -0700

    pycodestyle check

--- trainers/architectures.py
+++ trainers/architectures.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 """
 ## Class definitions for AutoEncoders on protein structural fragments ##
@@ -9,17 +9,18 @@ from torch import relu
 from torch.nn import Dropout, Linear, Module, ModuleList
 import torch.nn as nn
 
+
 class SimpleAEcnn(Module):
 	"""
 	Class definition for a simple CNN autoencoder working on protein fragments.
-	Following PyTorch AutoEncoder tutorial here: 
+	Following PyTorch AutoEncoder tutorial here:
 	https://analyticsindiamag.com/how-to-implement-convolutional-autoencoder-in-pytorch-with-cuda/.
-	All layers are convolutional. 
+	All layers are convolutional.
 	
 	Parameters
 	----------
 	dropout: Dropout rate (optional)
-		Optional dropout rate, between 0 and 1. 
+		Optional dropout rate, between 0 and 1.
 		float, 0 < dropout < 1
 	
 	Returns
@@ -27,71 +28,83 @@ class SimpleAEcnn(Module):
 	AutoEncoder model, PyTorch nn.Module object
 	"""
 	
-	#done for CA and the fragment size of 7
-	def __init__ (self, 
-				  dropout = None):
+	# done for CA and the fragment size of 7
+	def __init__(self, dropout=None):
 
-		if dropout != None:
+		if dropout is not None:
 			assert(type(dropout) == float)
 			assert(dropout < 1.0 and dropout > 0.0)
 		
 		super().__init__()
 		
-		#structure
+		# encoder
+		self.conv1 = nn.Conv2d(
+			in_channels=1,
+			out_channels=10,
+			kernel_size=2,
+			padding=3)
 		
-		#encoder
-		self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 10, 
-							   kernel_size = 2, padding = 3)
-		self.conv2 = nn.Conv2d(in_channels = 10, out_channels = 5, 
-							   kernel_size = 2, padding = 3)
+		self.conv2 = nn.Conv2d(
+			in_channels=10,
+			out_channels=5,
+			kernel_size=2,
+			padding=3)
 		
 		self.pool = nn.MaxPool2d(2, 2)
 		
-		#decoder
-		self.convt1 = nn.ConvTranspose2d(in_channels = 5, out_channels = 10, 
-										 kernel_size = 2, stride = 1)
-		self.convt2 = nn.ConvTranspose2d(in_channels = 10, out_channels = 1, 
-										 kernel_size = 2, stride = 1)
+		# decoder
+		self.convt1 = nn.ConvTranspose2d(
+			in_channels=5,
+			out_channels=10,
+			kernel_size=2,
+			stride=1)
 		
-		self.dropout = Dropout(dropout) if dropout != None else None
+		self.convt2 = nn.ConvTranspose2d(
+			in_channels=10,
+			out_channels=1,
+			kernel_size=2,
+			stride=1)
+		
+		self.dropout = Dropout(dropout) if dropout is not None else None
 	
 	def forward(self, features):
-		#conv1
+		# conv1
 		x = self.conv1(features)
 		x = relu(x)
 		x = self.pool(x)
-		if self.dropout != None: activate = self.dropout(x)
+		if self.dropout is not None: activate = self.dropout(x)
 		
-		#conv2 
+		# conv2
 		x = self.conv2(x)
 		x = relu(x)
 		x = self.pool(x)
-		if self.dropout != None: activate = self.dropout(x)
+		if self.dropout is not None: activate = self.dropout(x)
 		
-		#convtranspose1
+		# convtranspose1
 		x = self.convt1(x)
 		x = relu(x)
 		
-		#convtranspose2
+		# convtranspose2
 		x = self.convt2(x)
-		reconstructed = relu(x)		
+		reconstructed = relu(x)
 		
 		return reconstructed
+
 
 class SimpleAEfc(Module):
 	"""
 	Class definition for a simple autoencoder working on protein fragments.
 	Following PyTorch AutoEncoder tutorial here: https://gist.github.com/
 	AFAgarap/4f8a8d8edf352271fa06d85ba0361f26.
-	All layers are Fully Connected (FC) layers. 
+	All layers are Fully Connected (FC) layers.
 	
 	Parameters
 	----------
 	inshape: Input shape of fragment data
-		Model expects input fragment is a flattened array of coordinates. 
+		Model expects input fragment is a flattened array of coordinates.
 		inshape > 0
 	dropout: Dropout rate (optional)
-		Optional dropout rate, between 0 and 1. 
+		Optional dropout rate, between 0 and 1.
 		float, 0 < dropout < 1
 	
 	Returns
@@ -99,127 +112,123 @@ class SimpleAEfc(Module):
 	AutoEncoder model, PyTorch nn.Module object
 	"""
 	
-	def __init__(self,
-				 inshape=None,
-				 dropout=None):
+	def __init__(self, inshape=None, dropout=None):
 		
-		assert(inshape != None)
+		assert(inshape is not None)
 		assert(type(inshape) == int)
-		if dropout != None:
+		if dropout is not None:
 			assert(type(dropout) == float)
 			assert(dropout < 1.0 and dropout > 0.0)
 		
 		super().__init__()
 		
-		self.encoder_hidden = Linear(in_features=inshape,out_features=128)
+		self.encoder_hidden = Linear(in_features=inshape, out_features=128)
 		self.encoder_out    = Linear(in_features=128, out_features=10)
 		self.decoder_hidden = Linear(in_features=10, out_features=128)
 		self.decoder_out    = Linear(in_features=128, out_features=inshape)
 		
-		self.dropout = Dropout(dropout) if dropout != None else None
+		self.dropout = Dropout(dropout) if dropout is not None else None
 	
 	def forward(self, features):
 		"""
-		Run forward pass for model. 
+		Run forward pass for model.
 		Forward pass goes as follows ->
-			Input -> 128 FC hidden layer -> 10 FC latent code -> 
+			Input -> 128 FC hidden layer -> 10 FC latent code ->
 				128 FC hidden layer -> Input FC layer
 		Use torchinfo.summary to give summary of model
 		"""
 		activation = self.encoder_hidden(features)
-		if self.dropout != None: activation = self.dropout(activation)
+		if self.dropout is not None: activation = self.dropout(activation)
 		activation = relu(activation)
 		
 		latent = self.encoder_out(activation)
-		if self.dropout != None: latent = self.dropout(latent)
+		if self.dropout is not None: latent = self.dropout(latent)
 		latent = relu(latent)
 		
 		activation = self.decoder_hidden(latent)
-		if self.dropout != None: activation = self.dropout(activation)
+		if self.dropout is not None: activation = self.dropout(activation)
 		activation = relu(activation)
 		
 		reconstructed = self.decoder_out(activation)
 		
 		return reconstructed
 
+
 class DynamicAEfc(Module):
 	"""
 	Class definition for AutoEncoder model with dynamic number of layers and
-	units per layer. All layers are Fully Connected (FC) layers. 
+	units per layer. All layers are Fully Connected (FC) layers.
 	
 	Parameters
 	----------
 	inshape: Input shape of fragment data
-		Model expects input fragment is a flattened array of coordinates. 
+		Model expects input fragment is a flattened array of coordinates.
 		inshape > 0
 	units: list for units per layer, requred
-		list of units per layer, excluding size of input. 
+		list of units per layer, excluding size of input.
 	function_list: list for non-linear functions applied at each layer, requred
 		list of PyTorch non-linear functions to be applied at each layer.
-		len(function_list) needs to each length of units list.  
+		len(function_list) needs to each length of units list.
 	dropouts: list of dropout probabilities per layer
 		Not required.
-		If specified, must be list of equal length to units and function_list. 
+		If specified, must be list of equal length to units and function_list.
 	
 	Returns
 	-------
 	AutoEncoder model, PyTorch nn.Module object
 	"""
 	
-	def __init__(self,
-				inshape=None,
-				units=None,
-				function_list=None,
-				dropouts=None):
+	def __init__(
+		self,
+		inshape=None,
+		units=None,
+		function_list=None,
+		dropouts=None):
 	
-		assert(inshape != None and type(inshape) == int)
-		assert(units != None and type(units) == list)
-		assert(function_list != None and type(function_list) == list)
+		assert(inshape is not None and type(inshape) == int)
+		assert(units is not None and type(units) == list)
+		assert(function_list is not None and type(function_list) == list)
 		
 		assert(len(units) == len(function_list))
 		
-		if dropouts != None:
+		if dropouts is not None:
 			assert(type(dropout) == list)
 			assert(len(dropout) == len(units))
 			self.dropout = []
 			for dp in dropout:
 				assert(dp < 1.0 and dp > 0.0)
 				self.dropout.append(Dropout(dp))
-		else: self.dropout = [None]*len(units)
+		else: self.dropout = [None] * len(units)
 		
 		super().__init__()
 		self.inshape = inshape
 		self.units = units
 		self.funs = function_list
 		
-		#self.funs = [x() for x in function_list]
-		
 		self.linears = []
 		
 		prev = self.inshape
-		#print(prev)
 		for uu in units:
 			self.linears.append(Linear(prev, uu))
 			prev = uu
-			#print(prev)
-		#print(prev)
 		self.linears.append(Linear(prev, self.inshape))
 		self.linears = ModuleList(self.linears)
 	
 	def forward(self, x):
 		"""
 		Perform forward pass in the dynamic AutoEncoder model.
-		Use torchinfo.summary to find detailed summary of model. 
+		Use torchinfo.summary to find detailed summary of model.
 		"""
 		
 		out = x
-		for i in range(len(self.linears)-1):
+		for i in range(len(self.linears) - 1):
 			out = self.linears[i](out)
-			if self.dropout[i] != None: out = self.dropout[i](out)
+			if self.dropout[i] is not None: out = self.dropout[i](out)
 			out = self.funs[i](out)
 		reconstructed = self.linears[-1](out)
 		
 		return reconstructed
+
 
 if __name__ == '__main__':
 	
@@ -238,28 +247,34 @@ if __name__ == '__main__':
 	import torchvision
 	from training_tools import normalize_frag, distance_matrix, fit_model
 	
-	parser = argparse.ArgumentParser(description=''.join(
-									('Test training for PyTorch Model Class',
-									'definitions in this library')))
-	parser.add_argument('--df', '-d', required=True, type=str,
+	parser = argparse.ArgumentParser(
+		description='Test PyTorch Model definitions in library')
+	parser.add_argument(
+		'--df', '-d', required=True, type=str,
 		metavar='<path>', help='path to xz compressed pandas dataframe')
-	parser.add_argument('--split', '-s', required=False, type=float,
+	parser.add_argument(
+		'--split', '-s', required=False, type=float,
 		metavar='<float>', default=0.80, help='train/test split fraction')
-	parser.add_argument('--batchsize', '-b', required=False, type=int,
+	parser.add_argument(
+		'--batchsize', '-b', required=False, type=int,
 		metavar='<int>', default=1028, help='training batchsize')
-	parser.add_argument('--epochs', '-e', required=False, type=int,
+	parser.add_argument(
+		'--epochs', '-e', required=False, type=int,
 		metavar='<int>', default=5, help='num of epochs to run')
-	parser.add_argument('--lrate', '-l', required=False, type=float,
+	parser.add_argument(
+		'--lrate', '-l', required=False, type=float,
 		metavar='<float>', default=1e-3, help='learing rate')
-	parser.add_argument('--l2', '-w', required=False, type=float,
+	parser.add_argument(
+		'--l2', '-w', required=False, type=float,
 		metavar='<float>', default=0.0, help='l2 regularization weight')
-	parser.add_argument('--dropout', '-u', required=False, type=float,
+	parser.add_argument(
+		'--dropout', '-u', required=False, type=float,
 		metavar='<float>', default=None, help='dropout rate')
 	
 	arg = parser.parse_args()
 	
-	df = pd.read_pickle(arg.df, compression='xz').sample(frac=1.0,
-														 random_state=42)
+	df = pd.read_pickle(arg.df, compression='xz').sample(
+		frac=1.0, random_state=42)
 	
 	df['norm_frag'] = df.xyz_set.apply(normalize_frag)
 	df['dmatrix'] = df.xyz_set.apply(distance_matrix)
@@ -271,13 +286,13 @@ if __name__ == '__main__':
 	print(df.columns)
 	print(df.shape)
 	print()
-	trn = floor(df.shape[0]*arg.split)
+	trn = floor(df.shape[0] * arg.split)
 	
 	# setting seed to just compare the results
 	seed = 42
 	# setting the random seed from pytorch random number generators
 	torch.manual_seed(seed)
-	# enabling benchmark mode in cudnn (GPU accelerated library of primitives 
+	# enabling benchmark mode in cudnn (GPU accelerated library of primitives
 	# for deep neural net)
 	torch.backends.cudnn.benchmark = False
 	# making experiments reproducible
@@ -289,90 +304,119 @@ if __name__ == '__main__':
 	# create a model from `AE` autoencoder class
 	# load it to the specified device, either gpu or cpu
 	
-	model_aefc = SimpleAEfc(inshape=fshape,dropout=arg.dropout).to(device)
-	s_aefc = summary(model_aefc, input_size=(arg.batchsize,1,fshape), verbose=0)
+	model_aefc = SimpleAEfc(
+		inshape=fshape, dropout=arg.dropout).to(device)
+	s_aefc = summary(
+		model_aefc, input_size=(arg.batchsize, 1, fshape), verbose=0)
 	su_aefc = repr(s_aefc)
 	print(su_aefc.encode('utf-8').decode('latin-1'))
 	print()
+	
 	# Set the data loaders
 	train_coords = np.array(df.norm_frag[:trn].to_list())
 	test_coords  = np.array(df.norm_frag[trn:].to_list())
 	
-	train = data_utils.TensorDataset(torch.Tensor(train_coords), 
-									 torch.Tensor(train_coords))
-	train_loader = data_utils.DataLoader(train, 
-										 batch_size=arg.batchsize,
-										 shuffle=True)
-	test = data_utils.TensorDataset(torch.Tensor(test_coords),
-									torch.Tensor(test_coords))
-	test_loader = data_utils.DataLoader(test,batch_size=1,shuffle=True)
+	train = data_utils.TensorDataset(
+		torch.Tensor(train_coords),
+		torch.Tensor(train_coords))
+	
+	train_loader = data_utils.DataLoader(
+		train,
+		batch_size=arg.batchsize,
+		shuffle=True)
+	
+	test = data_utils.TensorDataset(
+		torch.Tensor(test_coords),
+		torch.Tensor(test_coords))
+	
+	test_loader = data_utils.DataLoader(test, batch_size=1, shuffle=True)
 	
 	# Set loss and optimizer
 	criterion = nn.L1Loss()
-	optimizer = optim.Adam(model_aefc.parameters(),
-						   lr=arg.lrate,
-						   weight_decay=arg.l2)
+	optimizer = optim.Adam(
+		model_aefc.parameters(),
+		lr=arg.lrate,
+		weight_decay=arg.l2)
 	
 	# Fit the model
-	model_aefc = fit_model(model_aefc,
-						   train=train_loader,
-						   test=test_loader,
-						   optimizer=optimizer,
-						   criterion=criterion,
-						   device=device,
-						   epochs=arg.epochs)
+	model_aefc = fit_model(
+		model_aefc,
+		train=train_loader,
+		test=test_loader,
+		optimizer=optimizer,
+		criterion=criterion,
+		device=device,
+		epochs=arg.epochs)
 	
-	model_dyn_aefc = DynamicAEfc(inshape=fshape,dropouts=arg.dropout,
-								 units = [128, 10, 128], 
-								 function_list = [relu, relu, relu]).to(device)
+	model_dyn_aefc = DynamicAEfc(
+		inshape=fshape,
+		dropouts=arg.dropout,
+		units=[128, 10, 128],
+		function_list=[relu, relu, relu]).to(device)
 	
-	s_dyn_aefc = summary(model_dyn_aefc, input_size=(arg.batchsize, 1, fshape), 
-						 verbose=0)
+	s_dyn_aefc = summary(
+		model_dyn_aefc,
+		input_size=(arg.batchsize, 1, fshape),
+		verbose=0)
 	su_dyn_aefc = repr(s_dyn_aefc)
 	print(su_dyn_aefc.encode('utf-8').decode('latin-1'))
 	print()
+	
 	# Set optimizer
-	optimizer = optim.Adam(model_dyn_aefc.parameters(),
-						   lr=arg.lrate,
-						   weight_decay=arg.l2)
+	optimizer = optim.Adam(
+		model_dyn_aefc.parameters(),
+		lr=arg.lrate,
+		weight_decay=arg.l2)
 	
 	# Fit the model
-	model_dyn_aefc = fit_model(model_dyn_aefc,
-							   train=train_loader,
-							   test=test_loader,
-							   optimizer=optimizer,
-							   criterion=criterion,
-							   epochs=arg.epochs)
+	model_dyn_aefc = fit_model(
+		model_dyn_aefc,
+		train=train_loader,
+		test=test_loader,
+		optimizer=optimizer,
+		criterion=criterion,
+		epochs=arg.epochs)
 	
 	model_aecnn = SimpleAEcnn(dropout=arg.dropout).to(device)
-	s_aecnn = summary(model_aecnn,
-					  input_size = (arg.batchsize, 1, dshape, dshape), 
-					  verbose = 0)
+	s_aecnn = summary(
+		model_aecnn,
+		input_size=(arg.batchsize, 1, dshape, dshape),
+		verbose=0)
+	
 	su_aecnn = repr(s_aecnn)
 	print(su_aecnn.encode('utf-8').decode('latin-1'))
 	print()
+	
 	# Set optimizer
-	optimizer = optim.Adam(model_aecnn.parameters(),
-						   lr=arg.lrate,
-						   weight_decay=arg.l2)
+	optimizer = optim.Adam(
+		model_aecnn.parameters(),
+		lr=arg.lrate,
+		weight_decay=arg.l2)
 	
 	# Set the data loaders
 	train_coords = np.array(df.dmatrix[:trn].to_list())
 	test_coords  = np.array(df.dmatrix[trn:].to_list())
 	
-	train = data_utils.TensorDataset(torch.Tensor(train_coords), 
-									 torch.Tensor(train_coords))
-	train_loader = data_utils.DataLoader(train, 
-										 batch_size=arg.batchsize,
-										 shuffle=True)
-	test = data_utils.TensorDataset(torch.Tensor(test_coords),
-									torch.Tensor(test_coords))
-	test_loader = data_utils.DataLoader(test,batch_size=1,shuffle=True)
+	train = data_utils.TensorDataset(
+		torch.Tensor(train_coords),
+		torch.Tensor(train_coords))
+	
+	train_loader = data_utils.DataLoader(
+		train,
+		batch_size=arg.batchsize,
+		shuffle=True)
+	
+	test = data_utils.TensorDataset(
+		torch.Tensor(test_coords),
+		torch.Tensor(test_coords))
+	
+	test_loader = data_utils.DataLoader(test, batch_size=1, shuffle=True)
 	
 	# Fit the model
-	model_aecnn = fit_model(model_aecnn,
-							train=train_loader,
-							test=test_loader,
-							optimizer=optimizer,
-							criterion=criterion,
-							epochs=arg.epochs)
+	model_aecnn = fit_model(
+		model_aecnn,
+		train=train_loader,
+		test=test_loader,
+		optimizer=optimizer,
+		criterion=criterion,
+		epochs=arg.epochs)
