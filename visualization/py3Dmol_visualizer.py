b9099110b698fe2ba03826718aa2349179515df2
Amalia <58994466+akaresh@users.noreply.github.com>
2021-08-02 15:51:28 -0700

    visualization updates

--- visualization/py3Dmol_visualizer.py
+++ visualization/py3Dmol_visualizer.py
@@ -16,17 +16,17 @@ def pdb_gen(row, dtype):
 	pdb_to_list = []
 
 	if dtype == 'xyz':
-		#print('initial')
+		print('initial')
 
 		for f_id, r, c in zip(row['fragment_ids'], row['fragment_seq'], row['xyz_set']):
 								#atom, fragment_id, atom_type, residue_3_letter_code, chain name,
 								#residue number, coordinates
 			#print(c)
-			pdb_to_list.append(['ATOM', (f_id), row['fragment_type'], aa_dict[r].upper(),
+			pdb_to_list.append(['ATOM', (f_id), row['fragment_type'], "XXX",
 								row['chain_id'], (f_id), (c[0]), (c[1]), (c[2])])
 
 	elif dtype == 'normalized':
-		#print('normalized')
+		print('normalized')
 
 		for f_id, r, c in zip(row['fragment_ids'], row['fragment_seq'], [row['norm_frag'][i:i+3] for i in
 			                  range(0, len(row['norm_frag']), 3)]):
@@ -91,12 +91,13 @@ if __name__ == '__main__':
 	#sys.exit()
 
 	for idx, frag_row in df.iterrows():
-		#initial = pdb_gen(frag_row, 'xyz')
-		normalized_frag = pdb_gen(frag_row, 'normalized')
+		initial = pdb_gen(frag_row, 'xyz')
+		#normalized_frag = pdb_gen(frag_row, 'normalized')
 		#after_cnn = pdb_gen(frag_row, 'cnn')
 		sys.exit()
 
 	###code for test examples
+
 
 
 
